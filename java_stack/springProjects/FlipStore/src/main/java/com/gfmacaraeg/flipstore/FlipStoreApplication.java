0c8180519131f51945055a0804598af2bec9b64e
gfmacaraeg <gianfranco.macaraeg@my.tccd.edu>
2018-05-29 04:23:23 -0500

    my first commit

---
+++ java_stack/springProjects/FlipStore/src/main/java/com/gfmacaraeg/flipstore/FlipStoreApplication.java
@@ -0,0 +1,12 @@
+package com.gfmacaraeg.flipstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlipStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlipStoreApplication.class, args);
+	}
+}
